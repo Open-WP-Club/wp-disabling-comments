f5a625b550aae956d8dc574a9f1e433c1b411777
MrGKanev <contact@mrgkanev.eu>
2024-08-19 11:10:46 +0300

    Update wp-disable-comments.php

--- wp-disable-comments.php
+++ wp-disable-comments.php
@@ -3,221 +3,139 @@
 /**
  * Plugin Name: Disable Comments
  * Description: Disables all WordPress comment functionality on the entire network.
- * Version: 0.0.2
- * GitHub Plugin URI: https://github.com/Open-WP-Club/wp-disabling-comments
+ * Version: 0.0.4
  * Author: Open WP Club
  * Author URI: https://github.com/Open-WP-Club
  * License: GPL2
  */
 
-namespace OpenWPClub\DisableComments;
+// Exit if accessed directly
+if (!defined('ABSPATH')) {
+    exit;
+}
 
-defined('ABSPATH') || exit;
-
-/**
- * Main class for disabling comments functionality.
- */
-class Disable_WP_Comments
+class OWPC_Disable_Comments
 {
-    /**
-     * Constructor.
-     */
+    private $options;
+
     public function __construct()
     {
-        // Initialize the plugin
-        add_action('plugins_loaded', [$this, 'init']);
+        add_action('plugins_loaded', array($this, 'init'));
     }
 
-    /**
-     * Initialize the plugin.
-     */
     public function init()
     {
-        // Load options
-        $this->options = get_option('owpc_disable_comments_options', [
+        $this->options = get_option('owpc_disable_comments_options', array(
             'disable_comments' => true,
             'disable_comments_frontend' => true,
             'disable_comments_dashboard' => true,
             'disable_comments_rest_api' => true,
-        ]);
+        ));
 
-        // These need to happen now
-        add_action('widgets_init', [$this, 'disable_rc_widget']);
-        add_filter('wp_headers', [$this, 'filter_wp_headers']);
-        add_action('template_redirect', [$this, 'filter_query'], 9); // before redirect_canonical
-
-        // Admin bar filtering has to happen here since WP 3.6
-        add_action('add_admin_bar_menus', [$this, 'filter_admin_bar'], 0);
-        add_action('admin_init', [$this, 'filter_admin_bar']);
-
-        // These can happen later
-        add_action('wp_loaded', [$this, 'setup_filters']);
-
-        // Add settings page
-        add_action('admin_menu', [$this, 'add_options_page']);
-        add_action('admin_init', [$this, 'register_settings']);
-
-        if ($this->options['disable_comments_frontend']) {
-            add_action('enqueue_block_editor_assets', [$this, 'filter_gutenberg_blocks']);
-        }
-
-        if ($this->options['disable_comments_rest_api']) {
-            add_filter('rest_endpoints', [$this, 'filter_rest_endpoints']);
-            add_filter('xmlrpc_methods', [$this, 'disable_xmlrc_comments']);
-            add_filter('rest_pre_insert_comment', [$this, 'disable_rest_api_comments'], 10, 2);
-        }
-
+        // Core filters
         add_filter('comments_array', '__return_empty_array', 20);
+        add_filter('comments_open', '__return_false', 20);
+        add_filter('pings_open', '__return_false', 20);
+
+        // Admin-side filters
+        if (is_admin()) {
+            add_action('admin_init', array($this, 'admin_init'));
+            add_action('admin_menu', array($this, 'admin_menu'), 9999);
+        }
+        // Frontend filters
+        else {
+            add_action('template_redirect', array($this, 'frontend_filters'));
+        }
+
+        // Common filters
+        add_filter('wp_headers', array($this, 'filter_wp_headers'));
+        add_action('widgets_init', array($this, 'disable_rc_widget'));
+
+        // REST API filters
+        add_filter('rest_endpoints', array($this, 'filter_rest_endpoints'));
+        add_filter('rest_pre_insert_comment', array($this, 'disable_rest_api_comments'), 10, 2);
+
+        // XML-RPC
+        add_filter('xmlrpc_methods', array($this, 'disable_xmlrpc_comments'));
     }
 
-    /**
-     * Set up filters based on options.
-     */
-    protected function setup_filters()
+    public function admin_init()
     {
-        if ($this->options['disable_comments']) {
-            $types = array_keys(get_post_types(['public' => true], 'objects'));
-            if (!empty($types)) {
-                foreach ($types as $type) {
-                    if (post_type_supports($type, 'comments')) {
-                        remove_post_type_support($type, 'comments');
-                        remove_post_type_support($type, 'trackbacks');
-                    }
+        // Remove comments metabox from dashboard
+        remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
+
+        // Disable support for comments and trackbacks in post types
+        foreach (get_post_types() as $post_type) {
+            if (post_type_supports($post_type, 'comments')) {
+                remove_post_type_support($post_type, 'comments');
+                remove_post_type_support($post_type, 'trackbacks');
+            }
+        }
+    }
+
+    public function admin_menu()
+    {
+        // Remove comments and discussion settings from admin menu
+        remove_menu_page('edit-comments.php');
+        remove_submenu_page('options-general.php', 'options-discussion.php');
+    }
+
+    public function frontend_filters()
+    {
+        // Remove comment-reply script for themes that include it indiscriminately
+        wp_deregister_script('comment-reply');
+        // Feed removals
+        remove_action('wp_head', 'feed_links_extra', 3);
+        add_filter('feed_links_show_comments_feed', '__return_false');
+    }
+
+    public function filter_wp_headers($headers)
+    {
+        unset($headers['X-Pingback']);
+        return $headers;
+    }
+
+    public function disable_rc_widget()
+    {
+        unregister_widget('WP_Widget_Recent_Comments');
+    }
+
+    public function filter_rest_endpoints($endpoints)
+    {
+        if ($this->options['disable_comments_rest_api']) {
+            foreach ($endpoints as $route => $endpoint) {
+                if (stripos($route, '/comments') !== false) {
+                    unset($endpoints[$route]);
                 }
             }
         }
+        return $endpoints;
+    }
 
-        // Filters for the admin only
-        if (is_admin() && $this->options['disable_comments_dashboard']) {
-            add_action('admin_menu', [$this, 'filter_admin_menu'], 9999);    // do this as late as possible
-            add_action('admin_print_styles-index.php', [$this, 'admin_css']);
-            add_action('admin_print_styles-profile.php', [$this, 'admin_css']);
-            add_action('wp_dashboard_setup', [$this, 'filter_dashboard']);
-            add_filter('pre_option_default_pingback_flag', '__return_zero');
+    public function disable_rest_api_comments($prepared_comment, $request)
+    {
+        if ($this->options['disable_comments_rest_api']) {
+            return new WP_Error(
+                'rest_comment_disabled',
+                __('Comments are disabled on this site.', 'disable-comments'),
+                array('status' => 403)
+            );
         }
-        // Filters for front end only
-        elseif ($this->options['disable_comments_frontend']) {
-            add_action('template_redirect', [$this, 'check_comment_template']);
-            add_filter('comments_open', '__return_false', 20);
-            add_filter('pings_open', '__return_false', 20);
-            add_filter('post_comments_feed_link', '__return_false');
-            add_filter('comments_link_feed', '__return_false');
-            add_filter('comment_link', '__return_false');
-            add_filter('get_comments_number', '__return_false');
-            add_filter('feed_links_show_comments_feed', '__return_false');
-        }
+        return $prepared_comment;
     }
 
-    // ... [All other methods from the original class remain here, 
-    //     but with 'protected' visibility where appropriate] ...
-
-    /**
-     * Add options page to the settings menu.
-     */
-    public function add_options_page()
+    public function disable_xmlrpc_comments($methods)
     {
-        add_options_page(
-            'Disable Comments Settings',
-            'Disable Comments',
-            'manage_options',
-            'disable-comments-settings',
-            [$this, 'render_options_page']
-        );
-    }
-
-    /**
-     * Register settings for the options page.
-     */
-    public function register_settings()
-    {
-        register_setting('owpc_disable_comments_options', 'owpc_disable_comments_options');
-
-        add_settings_section(
-            'owpc_disable_comments_section',
-            'Disable Comments Settings',
-            [$this, 'settings_section_callback'],
-            'disable-comments-settings'
-        );
-
-        add_settings_field(
-            'disable_comments',
-            'Disable Comments',
-            [$this, 'render_checkbox'],
-            'disable-comments-settings',
-            'owpc_disable_comments_section',
-            ['label_for' => 'disable_comments']
-        );
-
-        add_settings_field(
-            'disable_comments_frontend',
-            'Disable Comments on Frontend',
-            [$this, 'render_checkbox'],
-            'disable-comments-settings',
-            'owpc_disable_comments_section',
-            ['label_for' => 'disable_comments_frontend']
-        );
-
-        add_settings_field(
-            'disable_comments_dashboard',
-            'Disable Comments in Dashboard',
-            [$this, 'render_checkbox'],
-            'disable-comments-settings',
-            'owpc_disable_comments_section',
-            ['label_for' => 'disable_comments_dashboard']
-        );
-
-        add_settings_field(
-            'disable_comments_rest_api',
-            'Disable Comments in REST API',
-            [$this, 'render_checkbox'],
-            'disable-comments-settings',
-            'owpc_disable_comments_section',
-            ['label_for' => 'disable_comments_rest_api']
-        );
-    }
-
-    /**
-     * Render the options page.
-     */
-    public function render_options_page()
-    {
-?>
-        <div class="wrap">
-            <h1>Disable Comments Settings</h1>
-            <form action="options.php" method="post">
-                <?php
-                settings_fields('owpc_disable_comments_options');
-                do_settings_sections('disable-comments-settings');
-                submit_button();
-                ?>
-            </form>
-        </div>
-    <?php
-    }
-
-    /**
-     * Render a checkbox field.
-     *
-     * @param array $args Field arguments.
-     */
-    public function render_checkbox($args)
-    {
-        $options = get_option('owpc_disable_comments_options');
-    ?>
-        <input type="checkbox" id="<?php echo esc_attr($args['label_for']); ?>"
-            name="owpc_disable_comments_options[<?php echo esc_attr($args['label_for']); ?>]"
-            value="1" <?php checked(isset($options[$args['label_for']]) ? $options[$args['label_for']] : 0, 1); ?>>
-<?php
-    }
-
-    /**
-     * Settings section callback.
-     */
-    public function settings_section_callback()
-    {
-        echo '<p>Configure which comment-related features you want to disable.</p>';
+        unset($methods['wp.newComment']);
+        unset($methods['wp.getCommentCount']);
+        unset($methods['wp.getComment']);
+        unset($methods['wp.getComments']);
+        unset($methods['wp.deleteComment']);
+        unset($methods['wp.editComment']);
+        unset($methods['wp.newComment']);
+        unset($methods['wp.getCommentStatusList']);
+        return $methods;
     }
 }
 
-// Initialize the plugin
-new Disable_WP_Comments();
+new OWPC_Disable_Comments();
